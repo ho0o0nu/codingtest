5e7a196c4b5b6edd88100f03343fce5469833f8f
ho0o0nu <honu7283@gmail.com>
2025-05-31 17:54:59 +0900

    Time: 3 ms (91.1%), Space: 21.2 MB (61.64%) - LeetHub

--- LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,4 +1,14 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        ctr = Counter(nums)
-        return [i for i, v in ctr.most_common(k)]
+        freqs = collections.Counter(nums)
+        freqs_heap = []
+
+        # 힙에 음수로 삽입(최대값을 뽑기 위해)
+        for f in freqs:
+            heapq.heappush(freqs_heap, (-freqs[f], f))
+        
+        topk = list()
+        for _ in range(k):
+            topk.append(heapq.heappop(freqs_heap)[1])
+        
+        return topk
