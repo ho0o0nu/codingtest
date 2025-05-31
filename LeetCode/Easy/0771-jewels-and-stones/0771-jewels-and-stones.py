01f7431bef7674c20e9bfeceacd6c23769151f45
ho0o0nu <honu7283@gmail.com>
2025-05-31 16:35:10 +0900

    Time: 1 ms (13.72%), Space: 18 MB (10.87%) - LeetHub

--- LeetCode/Easy/0771-jewels-and-stones/0771-jewels-and-stones.py
+++ LeetCode/Easy/0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,16 +1,12 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        freqs = {}  # 해시 테이블
+        freqs = collections.defaultdict(int)
         count = 0
 
         for char in stones:
-            if char not in freqs:
-                freqs[char] = 1
-            else:
-                freqs[char] += 1
-
-        for char in jewels:
-            if char in freqs:
-                count += freqs[char]
+            freqs[char] += 1
         
+        for char in jewels:
+            count += freqs[char]
+
         return count
