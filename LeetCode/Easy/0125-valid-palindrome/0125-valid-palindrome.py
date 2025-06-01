913b5ce6c6dcd3241001c3e4f08f86675d22ae0a
ho0o0nu <honu7283@gmail.com>
2025-06-02 02:15:45 +0900

    Time: 7 ms (81.66%), Space: 18.8 MB (32.75%) - LeetHub

--- LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.py
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,14 +1,11 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-	    # 자료형 데크로 선언
-        strs: Deque = collections.deque()
-        
-        for char in s:
-            if char.isalnum():
-                strs.append(char.lower())
-        
-        while len(strs) > 1:
-            if strs.popleft() != strs.pop():
+        d = Deque()
+        cleaned = s.lower()
+        for ch in cleaned:
+            if ch.isalnum():
+                d.append(ch)
+        while len(d) > 1:
+            if d.popleft() != d.pop():
                 return False
-        
         return True
